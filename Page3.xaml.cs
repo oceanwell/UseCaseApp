4203a29e01f5867d1f9ed897724ff7c2c5840938
oceanwell <vd60405@gmail.com>
2025-10-27 18:57:22 +0300

    Add files via upload
    Добавлено скругление угол, теперь на тумблере можно добавлять каждый процент, плавное приближение и можно, нажимая на кнопку + на тумблере увеличивать каждые 5 процентов, на -, соответственно, уменьшать, скачивать проект нужно с этого branch

---
+++ Page3.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UseCaseApplication
+{
+    /// <summary>
+    /// Логика взаимодействия для Page3.xaml
+    /// </summary>
+    public partial class Page3 : Page
+    {
+        public Page3()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Закрываем окно, которое содержит эту страницу
+            Window parentWindow = Window.GetWindow(this);
+            if (parentWindow != null)
+            {
+                parentWindow.Close();
+            }
+        }
+    }
+}
